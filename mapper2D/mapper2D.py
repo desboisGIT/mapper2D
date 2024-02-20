edb3b90584f9d16ad236e2a97c6136b2a9163436
Robin Chaillou <90150299+desboisGIT@users.noreply.github.com>
2024-02-20 18:35:11 +0100

    Update mapper2D.py

--- mapper2D/mapper2D.py
+++ mapper2D/mapper2D.py
@@ -1,10 +1,14 @@
 import random
 
-def generateMap(width, groundMid, contrast):
+def generateLevels(width, groundMid, contrast,maxHeightFactor, minHeightFactor):
     heightList = []
     ht = groundMid
     for i in range(width):
         ht += random.randint(-contrast, contrast)
+        if ht >groundMid+maxHeightFactor:
+            ht -= contrast
+        if ht <groundMid-minHeightFactor:
+            ht += contrast
         heightList.append(ht)
     return heightList
 
@@ -14,9 +18,34 @@ def smooth(heightMap):
         smoothedMap[i] = (heightMap[i-1] + heightMap[i] + heightMap[i+1]) // 3
     return smoothedMap
 
-def mapper2D(width, groundMid, contrast, smoothingLevels):
-    heightMap = generateMap(width, groundMid, contrast)
+def makeHeightMap(width, groundMid, contrast, smoothingLevels,maxHeightFactor=10, minHeightFactor=1.5): #width and groundmid are in tiles
+    heightMap = generateLevels(width, groundMid, contrast, maxHeightFactor, minHeightFactor)
     for i in range(smoothingLevels):
         heightMap = smooth(heightMap)
     return heightMap
 
+def make2dMapArray(window_width, window_height, tileSize, heightMap):
+    nb = window_height // tileSize
+    tilesMap =[]
+    for x in range(window_width // tileSize):
+        tilesMap.append([])
+        for y in range(window_height // tileSize):
+            if y > nb - heightMap[x]:
+                blocktype = True  # True for solid blocks
+            else:
+                blocktype = False  # False for air blocks
+            tilesMap[x].append([x * tileSize, y * tileSize, blocktype])  # structure: xCord, yCord, blockType
+    return tilesMap
+
+
+def calculateVolume(tileList,x,y, deepth=300):
+    pass
+
+def calculateFluidLevel(heightMap):
+    sum = 0
+    for data in heightMap:
+        sum += data
+    return sum//len(heightMap)
+
+def fluidMaker(heightMap, ocurance):
+    fluidLevel = calculateFluidLevel(heightMap)
